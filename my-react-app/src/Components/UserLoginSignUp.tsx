d41c801d1dcdc3ee4f28c03d61627f33d3becd6f
Nusdha <nusdham-ps20203@stu.kln.ac.lk>
2025-02-26 14:01:09 +0530

    add photos to homepage

--- my-react-app/src/Components/UserLoginSignUp.tsx
+++ my-react-app/src/Components/UserLoginSignUp.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { addUser, getUserByEmail } from '../Service/MemberService';
-import { useAuth } from '../assets/Utils/AuthContext';
+import { useAuth } from '../Utils/AuthContext';
 import './LoginSignUp.css';
 
 interface User {
